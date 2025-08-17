a5668ccaa515837c8ebaf0f83c785822cb0bc040
moshika38 <moshika38@gmail.com>
2025-08-17 16:26:26 +0530

    fix e.u

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:ceylon_meditation_center/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
